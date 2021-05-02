9bf732be9bf67197529b5f1d19693f83bbd90e70
mei <axzs31587@gmail.com>
2021-05-03 00:46:27 +0800

    finished hw

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,8 @@
 function printStars(n) {
-
+  const str = '*'
+  for (let i = 0; i < n; i++) {
+    console.log(str)
+  }
 }
 
 printStars(5)
--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,14 @@
 function capitalize(str) {
-  
+  let newStr = ''
+  if (str[0] >= 'a' && str[0] <= 'z') {
+    newStr += str[0].toUpperCase()
+  } else {
+    newStr += str[0]
+  }
+  for (let i = 1; i < str.length; i++) {
+    newStr += str[i]
+  }
+  return newStr
 }
 
-console.log(capitalize('hello'));
+console.log(capitalize('hello'))
